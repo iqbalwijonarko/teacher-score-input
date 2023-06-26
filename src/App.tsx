d0a32cbd63c372521d0345bb05eddcb543a62db3
muhammad.iqbal <muhammad.iqbal@sirclo.com>
2023-06-26 21:54:42 +0700

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Rating {
+  [key: string]: {
+    [key: string]: number;
+  };
 }
+
+const initialRatings: Rating = {
+  'aspek_penilaian_1': {},
+  'aspek_penilaian_2': {},
+  'aspek_penilaian_3': {},
+  'aspek_penilaian_4': {}
+};
+
+const App: React.FC = () => {
+  const [ratings, setRatings] = useState<Rating>(initialRatings);
+
+  const handleRatingChange = (aspek: string, mahasiswa: string, value: number) => {
+    setRatings(prevRatings => ({
+      ...prevRatings,
+      [aspek]: {
+        ...prevRatings[aspek],
+        [mahasiswa]: value,
+      },
+    }));
+  };
+
+  const handleSave = () => {
+    const jsonOutput = JSON.stringify(ratings, null, 2);
+    console.log(jsonOutput);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', margin: '1rem 40rem' }}>
+      <h2>Aplikasi Penilaian Mahasiswa</h2>
+      <table style={{ display: "flex", justifyContent: 'center' }}>
+        <tbody>
+          <tr>
+            <td />
+            {Object.keys(ratings).map((aspek, index) => {
+              const penilaian = `Aspek penilaian ${index + 1}`
+              return (
+                <td key={aspek}>{penilaian}</td>
+              )
+            })}
+          </tr>
+          {[...Array(10)].map((_, index) => {
+            const mahasiswa = `Mahasiswa ${index + 1}`;
+            return (
+              <>
+                <tr style={{ border: '1px solid black' }}>
+                  <td key={mahasiswa}>{mahasiswa}</td>
+                  {Object.keys(ratings).map(aspek => (
+                    <td key={aspek}>
+                      <select
+                        value={ratings[aspek][mahasiswa] || ''}
+                        onChange={e => handleRatingChange(aspek, mahasiswa, Number(e.target.value))}
+                      >
+                        <option value="">-- Pilih --</option>
+                        {[...Array(10)].map((_, index) => (
+                          <option key={index + 1} value={index + 1}>{index + 1}</option>
+                        ))}
+                      </select>
+
+                    </td>
+                  ))}
+                </tr>
+              </>
+            );
+          })}
+        </tbody>
+      </table>
+      <div style={{ margin: '1rem', display: 'flex', justifyContent: 'flex-end' }}>
+        <button onClick={handleSave} style={{ border: '1px solid black', background: 'black', color: 'white', padding: '.1rem .4rem', fontWeight: 400 }}>Simpan</button>
+      </div>
+    </div>
+
+  );
+};
 
 export default App;
